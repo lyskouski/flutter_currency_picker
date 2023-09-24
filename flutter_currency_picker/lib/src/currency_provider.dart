6e179badbb4384d24556979258a10ab1e0cb4309
Viachaslau Lyskouski <deus@creativity.by>
2023-09-24 23:54:23 +0200

    [#149] Migrate from Fingrom. Drop flag converter

--- flutter_currency_picker/lib/src/currency_provider.dart
+++ flutter_currency_picker/lib/src/currency_provider.dart
@@ -6,13 +6,12 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_currency_picker/flutter_currency_picker.dart';
-import 'package:flutter_currency_picker/src/currency_defaults.dart';
 import 'package:flutter_currency_picker/src/source/crypto_list.dart';
 import 'package:flutter_currency_picker/src/source/currency_list.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 
-class CurrencyProvider {
+abstract class CurrencyProvider {
   static final _currencies = HashMap<String?, Currency>();
   static final List<Currency> _list = [];
 
@@ -21,18 +20,6 @@ class CurrencyProvider {
       _list.add(currency); //, flag: _toEmojiFlag(currency.flag)));
       _currencies[currency.code] = currency;
     }
-  }
-
-  // Return Emoji Unicode (Flag)
-  // See: https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
-  static String? toEmojiFlag(String? flag) {
-    if (flag == null) {
-      return null;
-    }
-    // 0x41 - 'A' symbol, 0x1F1E6 - Regional Indicator
-    const shift = -0x41 + 0x1F1E6;
-    return String.fromCharCode(flag.codeUnitAt(0) + shift) +
-        String.fromCharCode(flag.codeUnitAt(1) + shift);
   }
 
   // List of properties to Currency
